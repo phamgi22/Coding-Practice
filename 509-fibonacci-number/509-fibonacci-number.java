40c5317c337a9acf37d8e82d07dbde50c34ac64e
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-05-13 15:54:45 -0700

    Time: 0 ms (100.00%), Space: 41 MB (34.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,9 +1,27 @@
 class Solution {
     public int fib(int n) {
+//         // recursion solution
+//         if(n == 0) return 0;
+//         if(n == 1) return 1;
+        
+//         return fib(n-1) + fib(n-2);
+        
+        
+        // O(1) time complexity solution
         if(n == 0) return 0;
         if(n == 1) return 1;
         
-        return fib(n-1) + fib(n-2);
+        int a = 0;
+        int b = 1;
+        int sum = 0;
         
+        while(n > 1) {
+            sum = a + b;
+            a = b;
+            b = sum;
+            n--;
+        }
+
+        return sum;
     }
 }
