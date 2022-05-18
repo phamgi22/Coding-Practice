6f524fe1fea66c56320fdeb1d6fafe025bf5d0f4
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-05-17 21:17:09 -0700

    Time: 5 ms, Memory: 46 MB - LeetHub

---
+++ two-sum/two-sum.java
@@ -0,0 +1,84 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> theMap = new HashMap<>();
+        
+        for(int i=0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(theMap.containsKey(complement)) {
+                return new int[] {theMap.get(complement), i};
+            }
+            
+            theMap.put(nums[i], i);
+        }
+        
+        return null;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         // instantiate a new hash map 
+//         // for every element in nums array
+//         // calculate the difference between target and the element value
+//         // if map contain the complement of element value
+//         // return new int array {position of element complement, and position of element}
+        
+//         // if map does not contains the complement of the element value
+//         // add the element value as key with its position as value
+        
+// //         Map<Integer, Integer> map = new HashMap<>();
+        
+// //         for(int i = 0; i < nums.length - 1; i ++) {
+// //             int complement = target - nums[i];
+// //             if(map.containsKey(complement)) {
+// //                 return new int[] {map.get(complement), i};
+// //             }
+            
+// //             map.put(nums[i], i);
+// //         }
+        
+// //         return null;
+        
+        
+//         Map<Integer, Integer>  map = new HashMap<>(); 
+        
+//         for(int i=0; i < nums.length; i++) {
+//             int complement = target - nums[i];
+//             if(map.containsKey(complement)) {
+//                 return new int[]{map.get(complement), i};
+//             }
+            
+//             map.put(nums[i], i);
+//         }
+        
+//         return null;
+            
+    }
+}
