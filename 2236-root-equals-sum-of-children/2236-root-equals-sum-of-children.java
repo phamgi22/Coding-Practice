13791e6a9737827a1600a6bfb72b4ecb878bc4fa
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-06-22 22:31:44 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (27.60%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
@@ -14,18 +14,18 @@
  * }
  */
 
-public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+// public class TreeNode {
+//       int val;
+//       TreeNode left;
+//       TreeNode right;
+//       TreeNode() {}
+//       TreeNode(int val) { this.val = val; }
+//       TreeNode(int val, TreeNode left, TreeNode right) {
+//           this.val = val;
+//           this.left = left;
+//           this.right = right;
+//       }
+//   }
 class Solution {
     public boolean checkTree(TreeNode root) {
         return root.left.val + root.right.val == root.val;
