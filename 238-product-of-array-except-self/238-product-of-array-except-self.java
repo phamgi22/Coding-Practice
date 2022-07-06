8f5feba97f587ac57ee54ccd57c932747ff1ab14
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-07-06 12:36:36 -0700

    Time: 2 ms (87.25%), Space: 57.4 MB (74.66%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,23 +1,41 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
-        right[nums.length-1] = 1;
-        left[0] = 1;
+//         int[] left = new int[nums.length];
+//         int[] right = new int[nums.length];
+//         right[nums.length-1] = 1;
+//         left[0] = 1;
         
-        for (int  i=1; i<nums.length; i++) {
-            left[i] = nums[i-1] * left[i-1];
+//         for (int  i=1; i<nums.length; i++) {
+//             left[i] = nums[i-1] * left[i-1];
+//         }
+        
+//         for (int i=nums.length - 2; i>=0; i--) {
+//             right[i] = nums[i+1] * right[i+1];
+//         }
+        
+//         for (int i=0; i<nums.length; i++) {
+//             left[i] = left[i] * right[i];
+//         }
+        
+//         return left;
+        
+        
+        // output array does not count for the space complexity
+        
+        int[] answer = new int[nums.length];
+        answer[0] = 1;
+        int right = 1;
+        
+        for (int i=1; i<nums.length; i++) {
+            answer[i] = nums[i-1] * answer[i-1];
         }
         
-        for (int i=nums.length - 2; i>=0; i--) {
-            right[i] = nums[i+1] * right[i+1];
+        for (int i=nums.length - 1; i>=0; i--) {
+            answer[i] = answer[i] * right;
+            right *= nums[i];
         }
         
-        for (int i=0; i<nums.length; i++) {
-            left[i] = left[i] * right[i];
-        }
-        
-        return left;
-        
+        return answer;
+             
     }
 }
