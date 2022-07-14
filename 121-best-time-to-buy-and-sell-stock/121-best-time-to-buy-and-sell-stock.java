89a87e1f420b207e2db321f6078af78004c3ed22
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-07-13 21:12:13 -0700

    Time: 3 ms (67.17%), Space: 84.3 MB (24.27%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // using left pointer buy, initialize to 0
+        // using right pointer sell, inialize to 1, because sell day is 1 day after buy
+        // save a spot to save current calculated profit
+        // save a spot to save current max profit
+        
+        // iterating through each day in the input array starting from day 2
+        // if buy day price is less than sell day price, calculate profit  and max profit
+        // if sell day prices is less than buy day price,  update buy date to sell day, the cheaper
+        // increment selling day
+        
         int buy = 0;
         int sell = 1;
         int current = 0;
