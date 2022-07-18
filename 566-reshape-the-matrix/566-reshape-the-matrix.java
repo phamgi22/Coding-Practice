70d9d6de5abe1951ef8f214ae9d559e1b923aa92
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-07-18 01:19:24 -0700

    Time: 1 ms (89.33%), Space: 51 MB (24.20%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -1,43 +1,19 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-//         int rows = mat.length;
-//         int columns = mat[0].length;
-        
-//         if ((rows * columns) != (r * c)) return mat;
-        
-//         int row_num = 0;
-//         int column_num = 0;
-//         int[][] output = new int[r][c];
-        
-//         for (int i=0; i<rows; i++) {
-//             for (int j=0; j<columns; j++) {
-//                 output[row_num][column_num] = mat[i][j];
-//                 column_num++;
-//                 if (column_num == c) {
-//                     column_num = 0;
-//                 }
-                
-//                 row_num++;
-//             }
-//         }
-        
-//         return output;
-        
-        
         int rows = mat.length;
-        int column = mat[0].length;
+        int columns = mat[0].length;
         
-        if ((rows * column) != (r * c)) return mat;
+        if ((rows * columns) != (r * c)) return mat;
         
-        int[][] output = new int[r][c];
         int row_num = 0;
         int column_num = 0;
+        int[][] output = new int[r][c];
         
         for (int i=0; i<rows; i++) {
-            for (int j=0; j<column; j++) {
+            for (int j=0; j<columns; j++) {
                 output[row_num][column_num] = mat[i][j];
                 column_num++;
-                if(column_num == c) {
+                if (column_num == c) {
                     column_num = 0;
                     row_num++;
                 }
@@ -45,5 +21,6 @@ class Solution {
         }
         
         return output;
+        
     }
 }
