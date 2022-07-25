baf242852f535efee0548c5668e2f6194794b9f8
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-07-25 07:48:29 -0700

    Time: 5 ms (63.26%), Space: 46.2 MB (16.21%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -9,17 +9,16 @@ class Solution {
         // finally throw an exception if no match pair can be found
         
         
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i=0; i < nums.length; i++) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            
-            if (map.containsKey(complement)) {
-                return new int[] {map.get(complement), i};
+            if (!map.containsKey(complement)) {
+                map.put(nums[i], i);
+            } else {
+                return new int[] {i, map.get(complement)};
             }
-            
-            map.put (nums[i], i);
         }
         
-        throw new IllegalArgumentException("ss");
+        return new int[0];
     }
 }
