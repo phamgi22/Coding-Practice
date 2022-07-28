d979475b66fdb71f1ab545060fb80dec5f262349
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-07-28 16:57:37 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (90.79%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -6,13 +6,13 @@ class Solution {
         
         while (p2 >= 0) {
             if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[i--] = nums1[p1--];
-                // i--;
-                // p1--;
+                nums1[i] = nums1[p1];
+                i--;
+                p1--;
             } else {
-                nums1[i--] = nums2[p2--];
-                // i--;
-                // p2--;
+                nums1[i] = nums2[p2];
+                i--;
+                p2--;
             }
         }
     }
