e9c4e1207e2bc084c3fdbf40efd49a2db8083b8f
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-10 13:21:54 -0700

    Time: 9 ms (89.54%), Space: 55.8 MB (60.06%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        /*
+        Give:
+        - an array of strings
+        - group the anagrams together
+        - can return answer in any order
+        - anagrams = words that use the exact characters as each other but in different order
+        
+        
+        Algo1:
+        - create a return value of list of list of strings
+        - create a map that store and map the string values when sorted is the same as the sorted value
+        
+        */
+        
+        List<List<String>> groups = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
+        
+        for (String string : strs) {
+            char[] chars = string.toCharArray();
+            Arrays.sort(chars);
+            String sortedString = new String(chars);
+            
+            if (!map.containsKey(sortedString)) {
+                map.put(sortedString, new ArrayList<>());
+            }
+            
+            map.get(sortedString).add(string);
+        }
+        
+        groups.addAll(map.values());
+        return groups;
+    }
+}
