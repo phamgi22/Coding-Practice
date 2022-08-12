6d9afcaf076dc910c89743fe20a96e6ea4d3c4c9
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-12 13:45:14 -0700

    Time: 4 ms (77.46%), Space: 58 MB (22.35%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,32 +1,28 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        /*
-        Give:
-        - a m x n grid represents a map of 1(land) and 0(water)
-        - return number of island(1 which surrounded by water all four grid)
-        */
-        int count = 0;
+        
+        int numberOfIslands = 0;
         
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
-                    count++;
-                    bfs(grid, i, j);
+                    numberOfIslands += dfs (grid, i, j);
                 }
             }
         }
         
-        return count;
+        return numberOfIslands;
     }
     
-    
-    public void bfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >=grid[i].length || grid[i][j] == '0') return;
+    public int dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return 0;
         
         grid[i][j] = '0';
-        bfs(grid, i + 1, j);
-        bfs(grid, i - 1, j);
-        bfs(grid, i, j + 1);
-        bfs(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i , j + 1);
+        dfs(grid, i , j - 1);
+        
+        return 1;
     }
 }
