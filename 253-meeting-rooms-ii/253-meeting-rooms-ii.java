c82b5c1b376c49a50c32e1dbfedc4321f9801525
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-14 12:44:28 -0700

    Time: 15 ms (24.64%), Space: 45.5 MB (31.87%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,45 +1,25 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if (intervals.length == 0) {
-          return 0;
+        // sorting intervals by their start timee
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        int min = 0;
+        
+        for(int i = 0; i < intervals.length; i++){
+            if(pq.isEmpty() || intervals[i][0] < pq.peek()){
+                pq.add(intervals[i][1]);
+                min = Math.max(min, pq.size());
+            }
+            else{
+                while (!pq.isEmpty() && intervals[i][0] >= pq.peek())
+                    pq.poll();
+                pq.add(intervals[i][1]);
+            }
+            
         }
-
-        Integer[] start = new Integer[intervals.length];
-        Integer[] end = new Integer[intervals.length];
-
-        for (int i = 0; i < intervals.length; i++) {
-          start[i] = intervals[i][0];
-          end[i] = intervals[i][1];
-        }
-
-        // Sort the intervals by end time
-        Arrays.sort(
-            end,
-            new Comparator<Integer>() {
-              public int compare(Integer a, Integer b) {
-                return a - b;
-              }
-            });
-        Arrays.sort(
-            start,
-            new Comparator<Integer>() {
-              public int compare(Integer a, Integer b) {
-                return a - b;
-              }
-            });
-        int startPointer = 0, endPointer = 0;
-        int usedRooms = 0;
-        while (startPointer < intervals.length) {
-          if (start[startPointer] >= end[endPointer]) {
-            usedRooms -= 1;
-            endPointer += 1;
-          }
-
-          usedRooms += 1;
-          startPointer += 1;
-
-        }
-
-        return usedRooms;
-  }
+        
+        return min;
+    }
 }
