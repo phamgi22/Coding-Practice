7fa3ab66ab0940c50a4e0627fe79691ce76f7208
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-17 19:12:25 -0700

    Time: 9 ms (66.40%), Space: 43.2 MB (55.99%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -15,29 +15,36 @@
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<>();
-        if (root == null) return result;
+        // store the list of path
+        List<String> listPath = new ArrayList<>();
         
-        String current_path = Integer.toString(root.val);
-        if (root.left == null && root.right == null) result.add(current_path);
-        if (root.left != null) dfs(root.left, current_path, result);
-        if (root.right!= null) dfs(root.right, current_path, result);            
-        return result;
+        if (root == null) return listPath;
+        
+        String path = Integer.toString(root.val);
+
+        if (root.left == null && root.right == null) {
+            listPath.add(path);
+            return listPath;
+        }
+        
+        if (root.left != null) dfs(root.left, path, listPath);
+        if (root.right != null) dfs(root.right, path, listPath);
+        return listPath;
     }
     
-    
-    public void dfs(TreeNode node, String current_path, List<String> result) {
-        current_path += "->" + node.val;
+    // dfs()
+    // input: a node, a String path, a list of paths
+    // do: add no until reach leaf --- left to right
+    // output: none
+    private void dfs(TreeNode node, String path, List<String> listPath) {
+        path += "->" + node.val;
         
         if (node.left == null && node.right == null) {
-            result.add(current_path);
+            listPath.add(path);
             return;
         }
         
-        if (node.left != null) dfs(node.left, current_path, result);
-        if (node.right != null) dfs(node.right, current_path, result);
+        if (node.left != null) dfs(node.left, path, listPath);
+        if (node.right != null) dfs(node.right, path, listPath);
     }
-    
-    
-    
 }
