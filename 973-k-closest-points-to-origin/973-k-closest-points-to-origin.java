c061d34841e4f7bb7977e9b0a7ad518eb3bd5a6e
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-18 19:53:14 -0700

    Time: 47 ms (57.74%), Space: 118.2 MB (40.04%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,56 +1,22 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        return quickSelect(points, k);
-    }
-    
-    private int[][] quickSelect(int[][] points, int k) {
-        int left = 0, right = points.length - 1;
-        int pivotIndex = points.length;
-        while (pivotIndex != k) {
-            // Repeatedly partition the array
-            // while narrowing in on the kth element
-            pivotIndex = partition(points, left, right);
-            if (pivotIndex < k) {
-                left = pivotIndex;
-            } else {
-                right = pivotIndex - 1;
+        
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(   (a,b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])      );
+        
+        
+        System.out.println("Heap in the beginning: " + maxHeap);
+        for (int[] point : points) {
+            maxHeap.add(point);
+            if (maxHeap.size() > k) {
+                maxHeap.remove();
             }
         }
         
-        // Return the first k elements of the partially sorted array
-        return Arrays.copyOf(points, k);
-    }
-
-    private int partition(int[][] points, int left, int right) {
-        int[] pivot = choosePivot(points, left, right);
-        int pivotDist = squaredDistance(pivot);
-        while (left < right) {
-            // Iterate through the range and swap elements to make sure
-            // that all points closer than the pivot are to the left
-            if (squaredDistance(points[left]) >= pivotDist) {
-                int[] temp = points[left];
-                points[left] = points[right];
-                points[right] = temp; 
-                right--;
-            } else {
-                left++;
-            }
+        int[][] result = new int[k][2];
+        while (k-- > 0) {
+            result[k] = maxHeap.remove();
         }
         
-        // Ensure the left pointer is just past the end of
-        // the left range then return it as the new pivotIndex
-        if (squaredDistance(points[left]) < pivotDist)
-            left++;
-        return left;
-    }
-
-    private int[] choosePivot(int[][] points, int left, int right) {
-        // Choose a pivot element of the array
-        return points[left + (right - left) / 2];
-    }
-    
-    private int squaredDistance(int[] point) {
-        // Calculate and return the squared Euclidean distance
-        return point[0] * point[0] + point[1] * point[1];
+        return result;
     }
 }
