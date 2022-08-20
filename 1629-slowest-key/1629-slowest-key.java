5bb2efa239be0322e1c0efa32d8bb83f627f6270
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-19 18:58:57 -0700

    Time: 2 ms (43.59%), Space: 43.9 MB (82.69%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.java
+++ 1629-slowest-key/1629-slowest-key.java
@@ -27,28 +27,20 @@ class Solution {
         
 //         return slowestKey;
         
-        HashMap<Character, Integer> durationMap = new HashMap<>();
-        durationMap.put(keysPressed.charAt(0), releaseTimes[0]);
-        // find and store the keypress duration for each key in the durationMap
-        for (int i = 1; i < releaseTimes.length; i++) {
-            int currentDuration = releaseTimes[i] - releaseTimes[i - 1];
+        int n = releaseTimes.length;
+        int longestDur = releaseTimes[0];
+        char slowestKey = keysPressed.charAt(0);
+        
+        for (int i = 1; i < n; i++) {
+            int currentDur = releaseTimes[i] - releaseTimes[i-1];
             char currentKey = keysPressed.charAt(i);
-            durationMap.put(currentKey,
-                Math.max(durationMap.getOrDefault(currentKey, 0), currentDuration));
-        }
-        char slowestKey = ' ';
-        int longestPressDuration = 0;
-        // iterate over the map to find the slowest key
-        for (Map.Entry mapElement : durationMap.entrySet()) {
-            int duration = (int) mapElement.getValue();
-            char key = (char) mapElement.getKey();
-            if (duration > longestPressDuration) {
-                longestPressDuration = duration;
-                slowestKey = key;
-            } else if (duration == longestPressDuration && key > slowestKey) {
-                slowestKey = key;
+            
+            if (currentDur > longestDur || currentDur == longestDur && currentKey > slowestKey) {
+                slowestKey = currentKey;
+                longestDur = currentDur;
             }
         }
+        
         return slowestKey;
     }
 }
