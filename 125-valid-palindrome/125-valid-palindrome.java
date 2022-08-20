ce9b42d82aa10db53812b123c5aa0089b927a73d
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-20 03:26:57 -0700

    Time: 9 ms (49.76%), Space: 44.5 MB (41.97%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,56 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // initiate a String to store the input string removed punctuation
-        // iterating through each character in the input String
-        //          if the character is digit or is a letter then add to removed punctuation S
-        // toLowercase the removed String
-        // iterate backward to validate  valindrome of removed String (two pointers)
-        
-        
-//         String sRemoved = "";
-//         for (char c : s.toCharArray()) {
-//             if (Character.isDigit(c) || Character.isLetter(c)) {
-//                 sRemoved += c;
-//             }
-//         }
-        
-//         sRemoved = sRemoved.toLowerCase();
-//         int left_Pointer = 0;
-//         int right_Pointer = sRemoved.length() - 1;
-        
-//         while (left_Pointer <= right_Pointer) {
-//             if (sRemoved.charAt(left_Pointer) != sRemoved.charAt(right_Pointer)) {
-//                 return false;
-//             }
-            
-//             left_Pointer++;
-//             right_Pointer++;
-//         }
-        
-//         return true;
-        
-        String fixed_string = "";
+        StringBuilder sb = new StringBuilder();
         
         for (char c : s.toCharArray()) {
             if (Character.isLetterOrDigit(c)) {
-                fixed_string += c;
-            }
-        }
-                
-        fixed_string = fixed_string.toLowerCase();
-        
-        int a = 0;
-        int b = fixed_string.length() - 1;
-        
-        while (a <= b) {
-            if (fixed_string.charAt(a) != fixed_string.charAt(b)) {
-                return false;
+                sb.append(Character.toLowerCase(c));
             }
             
-            a++;
-            b--;
         }
-                
-        return true;        
+        
+        System.out.println(sb.toString());
+        
+        return sb.toString().equals(sb.reverse().toString());
     }
 }
