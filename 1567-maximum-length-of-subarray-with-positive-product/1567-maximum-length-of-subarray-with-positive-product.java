03d8b521afe1159cbe324c995b466941cdf679ad
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-24 16:45:36 -0700

    Time: 12 ms (10.04%), Space: 77.1 MB (76.38%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
@@ -1,40 +1,30 @@
 class Solution {
     public int getMaxLen(int[] nums) {
-        /*
-        */
-        
-        // final max length of subarray that we are going to return
-        int max = 0;
-        
-        // an outer for loop
-        for (int i = 0; i < nums.length; i++) {
-            if (nums.length - i < max) {
-                return max;
-            }
-            // check if the number is a non-zero number
-            if (nums[i] != 0) {
-                // count the negative number, if nums[i] > 0, neg = 0
-                //                            if nums[i] < 0, neg = 1
-                int negatives = nums[i] > 0 ? 0 : 1;
-                // update max subarray length,if nums[i] > 0, right arg = 1
-                //                            if nums[i] < 0, right arg = 0
-                max = Math.max(max, nums[i] > 0 ? 1 : 0);
-                // innter loop, starting from the element on the right on i
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] < 0) {
-                        negatives++;
-                    } else if (nums[j] == 0) {
-                        break;
-                    }
-                    if (negatives % 2 == 0) {
-                        max = Math.max(max, j - i + 1);
-                    }
+            int maxLength = 0;
+            int index = 0;
+            while (index < nums.length) {
+                int j = index;
+                int negatives = 0;
+                while (j < nums.length && nums[j] != 0) {
+                    if (nums[j] < 0) negatives++;
+                    if (negatives % 2 == 0) maxLength = Math.max(maxLength, j - index + 1);
+                    j++;
                 }
-                
+                index = j + 1;
             }
+        
+        index = nums.length - 1;
+        while (index >= 0) {
+                int j = index;
+                int negatives = 0;
+                while (j >= 0 && nums[j] != 0) {
+                    if (nums[j] < 0) negatives++;
+                    if (negatives % 2 == 0) maxLength = Math.max(maxLength, index - j + 1);
+                    j--;
+                }
+                index = j - 1;
         }
         
-        return max;
-        
+        return maxLength;
     }
 }
