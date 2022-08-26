c216d3408202040f30ba41a4941de0231e6395c2
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-26 03:31:15 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (87.26%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
+            sum[i] = sum[i-1] + nums[i];
         }
         
-        return nums;
+        return sum;
     }
 }
