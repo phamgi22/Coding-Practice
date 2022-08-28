43996980a1b0faf6f5366b14f563177203e30aa6
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-28 00:31:16 -0700

    Time: 5 ms (73.55%), Space: 44.5 MB (42.66%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,31 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())  return false;
-        // anagrams: words which contains the exact same characters but in diff order
-        
-        
-//         // Sorting approach:
-//         char[] sC = s.toCharArray();
-//         char[] tC = t.toCharArray();
-//         Arrays.sort(sC);
-//         Arrays.sort(tC);
-//         return Arrays.equals(sC,tC);
-        
-        
-        // Frequency counter approach
-        int[] charCount = new int[26];             // bc there are 26 character in alphabet
-        for (int i = 0; i<s.length(); i++) {
-            charCount[s.charAt(i) - 'a']++;
-            charCount[t.charAt(i) - 'a']--;
-        }
-        
-        for (int count : charCount) {
-            if (count != 0) {
-                return false;
-            }
-        }
-        
-        return true;
-        
+        char[] sSorted = s.toCharArray();
+        Arrays.sort(sSorted);
+        char[] tSorted = t.toCharArray();
+        Arrays.sort(tSorted);
+        return Arrays.equals(sSorted,tSorted);
     }
 }
