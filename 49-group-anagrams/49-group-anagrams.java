c0ca9131c33fc1befcdd3f4dfbd58135a9a463d1
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-30 01:25:55 -0700

    Time: 12 ms (69.80%), Space: 55.7 MB (65.22%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,19 +1,31 @@
 class Solution {
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        if (strs.length == 0) return res;
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            char[] hash = new char[26];
-            for (char c : s.toCharArray()) {
-                hash[c - 'a']++;
+        /*
+        Give: 
+        - an array of strings named strs
+        - group the anagrams together
+        - return answer in any order
+        
+        - a hashmap: key - sorted string
+                     value - when sorted equal to key
+        */
+        
+        
+        Map<String,List<String>> map = new HashMap<>();
+        for (String string : strs) {
+            char[] current = string.toCharArray();
+            Arrays.sort(current);
+            String sorted = new String(current);
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+                map.get(sorted).add(string);
+            } else {
+                map.get(sorted).add(string);
             }
-            String key = new String(hash);
-            map.computeIfAbsent(key, k -> new ArrayList<>());
-            map.get(key).add(s);
         }
-        res.addAll(map.values());
-        return res;
+        
+        List<List<String>> output = new ArrayList<>();
+        output.addAll(map.values());
+        return output;
     }
 }
