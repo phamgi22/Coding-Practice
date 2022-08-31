7473ef443e4e00d5ad59efafcd36b45cb0be02bd
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-31 10:03:45 -0700

    Time: 2 ms (71.09%), Space: 41.8 MB (15.16%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -3,6 +3,7 @@ class Solution {
         if (n == 0) return false;
         if (n == 1) return true;
         if (n < 0) return false;
+        
         while (n >= 2) {
             if (n % 2 != 0) return false;
             n /= 2;
