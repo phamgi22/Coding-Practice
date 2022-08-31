7470905956693fc9a84b1c300f890d48751a4c32
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-08-31 10:04:00 -0700

    Time: 1 ms (100.00%), Space: 42 MB (10.86%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n == 0) return false;
+        if (n <= 0) return false;
         if (n == 1) return true;
-        if (n < 0) return false;
         
         while (n >= 2) {
             if (n % 2 != 0) return false;
