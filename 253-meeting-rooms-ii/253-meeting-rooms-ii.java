a67a6b8c42199c7fe5c396bd070e54eefa83284b
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-02 08:23:41 -0700

    Time: 71 ms (5.10%), Space: 51.3 MB (5.39%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,25 +1,21 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        // sorting intervals by their start timee
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        int min = 0;
+        pq.add(intervals[0][1]);
         
-        for(int i = 0; i < intervals.length; i++){
-            if(pq.isEmpty() || intervals[i][0] < pq.peek()){
+        for (int i = 1; i < intervals.length; i++) {
+            System.out.println(pq);
+            if (intervals[i][0] < pq.peek()) {
                 pq.add(intervals[i][1]);
-                min = Math.max(min, pq.size());
-            }
-            else{
-                while (!pq.isEmpty() && intervals[i][0] >= pq.peek())
-                    pq.poll();
+            } else {
+                pq.poll();
                 pq.add(intervals[i][1]);
             }
-            
         }
         
-        return min;
+        return pq.size();
     }
 }
