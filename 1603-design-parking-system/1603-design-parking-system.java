84f212098199a68c1d35c3eb03e8c5cbacc90485
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-07 19:18:41 -0700

    Time: 17 ms (13.24%), Space: 54.5 MB (47.76%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,46 +1,36 @@
 class ParkingSystem {
-
     int[] spaces = new int[3];
     int[] parked = new int[3];
     
     public ParkingSystem(int big, int medium, int small) {
-        spaces[0] = big;
-        spaces[1] = medium;
-        spaces[2] = small;
+        this.spaces[0] = big;
+        this.spaces[1] = medium;
+        this.spaces[2] = small;
     }
     
     public boolean addCar(int carType) {
-        switch(carType){
-            case 1:
-                parked[0]++;
-                if(parked[0]>spaces[0]) return false;
+        switch(carType) {
+            case 1: 
+                this.parked[0]++;
+                if (parked[0]>spaces[0]) return false;
                 break;
-            case 2:
-                parked[1]++;
-                if(parked[1]>spaces[1]) return false;
-                break; 
-            case 3:
-                parked[2]++;
-                if(parked[2]>spaces[2]) return false;
+            case 2: 
+                this.parked[1]++;
+                if (parked[1]>spaces[1]) return false;
+                break;
+            case 3: 
+                this.parked[2]++;
+                if (parked[2]>spaces[2]) return false;
                 break;
         }
+        
         
         return true;
     }
 }
+
 /**
  * Your ParkingSystem object will be instantiated and called as such:
  * ParkingSystem obj = new ParkingSystem(big, medium, small);
  * boolean param_1 = obj.addCar(carType);
  */
-
-
-/*
-Give:
-- design a parking system for a parking lot
-- three kinds of spaces: big, medium and small
-- a fixed (specific) number oof slots for each space size
-
-- implement the ParkingSystem class:
-- addCar(int carType): checks whether there is a parking space of carType
-*/
