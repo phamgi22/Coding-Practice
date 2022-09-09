12b664c84643aff1edfab39ce39a1cb91a583e8b
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-08 17:35:40 -0700

    Time: 12 ms (46.61%), Space: 45.7 MB (25.48%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -2,19 +2,77 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
+        // System.out.println(Arrays.deepToString(intervals));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        pq.add(intervals[0][1]);
         
+        pq.add(intervals[0][1]);
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] < pq.peek()) {
                 pq.add(intervals[i][1]);
-            } else {
+            } else if (intervals[i][0] >= pq.peek()) {
                 pq.poll();
                 pq.add(intervals[i][1]);
             }
         }
-        
         return pq.size();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+//         pq.add(intervals[0][1]);
+        
+//         for (int i = 1; i < intervals.length; i++) {
+//             if (intervals[i][0] < pq.peek()) {
+//                 pq.add(intervals[i][1]);
+//             } else {
+//                 pq.poll();
+//                 pq.add(intervals[i][1]);
+//             }
+//         }
+        
+//         return pq.size();
     }
 }
