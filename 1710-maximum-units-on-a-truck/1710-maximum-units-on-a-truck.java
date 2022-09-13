321e40d5662e0c27a9ae6090b4688e99c66c5ed2
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-12 18:00:47 -0700

    Time: 18 ms (27.44%), Space: 50.9 MB (45.19%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,27 +1,16 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        /*
-        Given:
-        - assigned to put some amount of boxes onto one truck
-        - 2d array boxtypes
-        - boxtypes[i] = [numberOfBoxes, numberOfUnitsPerBox]
-        
-        - integer truckSize, the  max number of boxes that can be put on the truck
-        - can choose any  boxes  to  put on the truck  as long as the number of boxes does not exceed truckSize
-        - return max units can be put on truck
-        */
-        
-        Arrays.sort(boxTypes, (a,b) -> b[1] - a[1]);
-        
-        int unitCount = 0;
+        Arrays.sort(boxTypes, (a,b) -> (b[1] - a[1]));
+        int maxUnit = 0;
         
         for (int[] boxType : boxTypes) {
-            int boxCount = Math.min(truckSize, boxType[0]);
-            unitCount += boxCount * boxType[1];
-            truckSize -= boxCount;
+            int boxes = Math.min(boxType[0], truckSize);
+            maxUnit += boxes * boxType[1];
+            truckSize -= boxes;
+            
             if (truckSize == 0) break;
         }
-        
-        return unitCount;
+      
+        return maxUnit;
     }
 }
