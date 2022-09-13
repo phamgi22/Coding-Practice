57caaee8dc427e37b7cf902025b97fb90fbb9916
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-12 17:39:12 -0700

    Time: 4 ms (79.14%), Space: 45.4 MB (44.34%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,30 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        /*
-        Give:
-        - array of integers nums
-        - integer target
-        - return indices of the two numbers that added up to target
-        - can return in any order
-        - is it guarantee that there will be a correct answer
-        
-        - for each number in array
-        - search in map if there's a number x where x + number = target
-        
-        */
-        
         Map<Integer,Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if (!map.containsKey(complement)) {
-                map.put(nums[i], i);
-            } else {
-                return new int[] {i, map.get(complement)};
-            }
+            
+            if (!map.containsKey(complement)) map.put(nums[i], i);
+            else return new int[]{i, map.get(complement)};
         }
         
-        return new int[0];
         
+        return new int[2];
     }
 }
