3bd3f01a140cd1105468dec2cacf20a4545b8bc9
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-16 22:54:54 -0700

    Time: 1 ms (80.08%), Space: 51.4 MB (33.17%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,11 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        if (nums[0] != 0) return 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i+1] - nums[i] != 1) return nums[i] + 1; 
+        int trueSum = 0;
+        
+        for (int i = 0; i <= nums.length; i++) {
+            trueSum += i;
         }
         
-        return nums[nums.length - 1] + 1;
+        int numsSum = 0;
+        
+        for (int num : nums) {
+            numsSum += num;
+        }
+        
+        return trueSum - numsSum;
     }
 }
