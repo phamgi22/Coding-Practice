119d5d4b2ff965c07d3179e408e9f8ff32fa8eb7
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-16 22:45:17 -0700

    Time: 6 ms (20.57%), Space: 46.4 MB (55.19%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -14,20 +14,22 @@
  * }
  */
 class Solution {
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-        if (s == null) {
+    public boolean isSubtree(TreeNode root, TreeNode sub) {
+        if (root == null) {
             return false;
-        } else if (isSameTree(s, t)) {
+        } else if (isSameTree(root, sub)) {
             return true;
-        } else return isSubtree(s.left, t) || isSubtree(s.right, t);
+        } else {
+            return isSubtree(root.right, sub) || isSubtree(root.left, sub);
+        }
     }
     
-    public boolean isSameTree(TreeNode s, TreeNode t) {
-        if (s == null || t == null) {
-            return s == null && t == null;
-        } else if (s.val == t.val) {
-            return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
-        } else {
+    public boolean isSameTree(TreeNode root, TreeNode sub) {
+        if (root == null || sub == null) {
+            return root == null && sub == null;
+        } else if (root.val == sub.val) {
+            return isSameTree(root.right, sub.right) && isSameTree(root.left, sub.left);
+        } else  {
             return false;
         }
     }
