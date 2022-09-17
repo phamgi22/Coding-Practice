e000fff6075f7782dd6b64ba39e5617ca593d85b
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-16 22:42:47 -0700

    Time: 6 ms (20.57%), Space: 46.8 MB (31.32%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -19,16 +19,14 @@ class Solution {
             return false;
         } else if (isSameTree(s, t)) {
             return true;
-        } else {
-            return isSubtree(s.left, t) || isSubtree(s.right, t);
-        }
+        } else return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
     
-    private boolean isSameTree(TreeNode s, TreeNode t) {
+    public boolean isSameTree(TreeNode s, TreeNode t) {
         if (s == null || t == null) {
             return s == null && t == null;
         } else if (s.val == t.val) {
-            return isSameTree(s.right, t.right) && isSameTree(s.left, t.left);
+            return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
         } else {
             return false;
         }
