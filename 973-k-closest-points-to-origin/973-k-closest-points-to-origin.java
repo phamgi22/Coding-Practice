ca7d192ced648b8bbb73c5e68866de44b6a88ce4
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-19 02:35:02 -0700

    Time: 74 ms (28.92%), Space: 116.6 MB (61.60%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,19 +1,38 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>
-            ((a,b)->((b[1]*b[1]+b[0]*b[0])-(a[1]*a[1]+a[0]*a[0])));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> 
+            ((b[0]*b[0]+b[1]*b[1]) - (a[0]*a[0]+a[1]*a[1])));
         
         for (int[] point : points) {
             pq.add(point);
             if (pq.size() > k) pq.poll();
         }
         
+        
         int[][] res = new int[k][2];
         
         while (k-- > 0) {
-            res[k] = (pq.poll());
+            res[k] = pq.poll();
         }
         
         return res;
+        
+        
+        
+//         PriorityQueue<int[]> pq = new PriorityQueue<>
+//             ((a,b)->((b[1]*b[1]+b[0]*b[0])-(a[1]*a[1]+a[0]*a[0])));
+        
+//         for (int[] point : points) {
+//             pq.add(point);
+//             if (pq.size() > k) pq.poll();
+//         }
+        
+//         int[][] res = new int[k][2];
+        
+//         while (k-- > 0) {
+//             res[k] = (pq.poll());
+//         }
+        
+//         return res;
     }
 }
