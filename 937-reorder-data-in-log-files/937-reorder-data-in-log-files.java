a8fff10972289c2f20a4db422d246f73ab51df62
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-18 19:45:06 -0700

    Time: 9 ms (64.24%), Space: 47 MB (57.08%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -2,56 +2,54 @@ class Solution {
     public String[] reorderLogFiles(String[] logs) {
         /*
         Rules:
-        A. 2 letter log
-            a. main lexicographically sort
-            b. if main is same, id lexicographically sort
-        B. 1 letter + 1 digit log: letter always come first
-        C. 2 digit log: main tain index order in logs
+        1. letter come before digit
+        2. letter sort lexicographically
+        3. if main portion is same in two log, return id portion lexicographically
+        4. maintain order of index for digit log
+        
+        
+        Cases:
+        1. 2 letter
+            a. main portion diff, sort main lexicographically
+            b. main portion same, sort id lexicographically
+        2. 1 letter + 1 digit: letter always come first
+        3. 2 digit: maintain order, 0
         */
         
-        Arrays.sort(logs, (log1, log2) -> {
-            // log 1 < log 2 return -1
-            // log 1 = log 2 return 0
-            // log 1 > log s return 1
+        Arrays.sort(logs, (log1,log2) -> {
+            // log1 < log2 return -1;
+            // log1 == log 2 return 0:
+            // log1 > log2 return 1
             
-            // getting the index of the first space
             int index1 = log1.indexOf(" ");
-            
-            // extract indentifier of the log
             String id1 = log1.substring(0, index1);
-            
-            // extract main portion of the log
             String main1 = log1.substring(index1 + 1);
             
             int index2 = log2.indexOf(" ");
             String id2 = log2.substring(0, index2);
             String main2 = log2.substring(index2 + 1);
             
-            // check if the log is a digit log
+            
             boolean isDigit1 = Character.isDigit(main1.charAt(0));
             boolean isDigit2 = Character.isDigit(main2.charAt(0));
             
-            
-            // case 1: both log are letter log
             if (!isDigit1 && !isDigit2) {
-                // case 1a: check if main portion is lexicographically sort
                 int value = main1.compareTo(main2);
-                if (value == 0) return id1.compareTo(id2); // case 1b
+                if (value == 0) return id1.compareTo(id2);
                 return value;
             }
             
-            // at this point there is at least 1 digit log
+            
             if (isDigit1) {
                 if (isDigit2) {
-                    return 0; // both log
+                    return 0;
                 } else {
+                    // digit2 a letter, so it come before digit1, digit come after
                     return 1;
                 }
             } else {
                 return -1;
             }
-            
-            
         });
         
         return logs;
