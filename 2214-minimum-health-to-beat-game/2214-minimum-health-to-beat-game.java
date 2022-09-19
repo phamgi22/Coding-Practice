085ba62ff871d1e47c996e74f788747c28550f00
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-19 10:48:01 -0700

    Time: 4 ms (49.22%), Space: 75.1 MB (73.28%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.java
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.java
@@ -1,17 +1,14 @@
-// Add all the damages to find required health (Add one to the result because the health should be greater than zero at any time)
-// Find the maximum number out of damage array to determine which level to use the armor in.
-// To optimally use the armor, we need to use it against the level that causes the most damage. Subtract this from the health to get the minimum required health.
 class Solution {
     public long minimumHealth(int[] damage, int armor) {
         long max_damage = 0;
         long min_health = 1;
         
-        for (int i = 0; i < damage.length; i++) {
+        for (int i = 0 ; i < damage.length; i++) {
             min_health += damage[i];
             max_damage = Math.max(max_damage, damage[i]);
         }
         
-        min_health -= Math.min(armor, max_damage);
+        min_health -= Math.min(max_damage, armor);
         return min_health;
     }
 }
