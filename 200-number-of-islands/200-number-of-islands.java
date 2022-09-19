fe47132c29fc89fde740be965e66fd1f7301b7c0
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-19 02:40:28 -0700

    Time: 3 ms (97.48%), Space: 51.1 MB (88.63%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,30 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int islands = 0;
+        if (grid == null || grid.length == 0) return 0;
+        
+        int count = 0;
         
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
-                    islands++;
+                    count++;
                     callBFS(grid, i, j);
                 }
             }
         }
         
-        return islands;
+        return count;
     }
     
-    private void callBFS(char[][] grid, int i, int j) {
-        // out of bound OR reach the end of the island ===> break
+    
+    public void callBFS(char[][] grid, int i, int j) {
         if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[i].length - 1 || grid[i][j] == '0') {
             return;
         }
         
+        
         grid[i][j] = '0';
         
-        callBFS(grid, i - 1, j);
         callBFS(grid, i + 1, j);
-        callBFS(grid, i, j+1);
-        callBFS(grid, i, j-1);
+        callBFS(grid, i - 1, j);
+        callBFS(grid, i, j + 1);
+        callBFS(grid, i, j - 1);
     }
 }
