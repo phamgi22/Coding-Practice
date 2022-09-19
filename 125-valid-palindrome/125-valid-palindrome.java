d879a0c26fc9d9120fac7765d90bd86e9a190ce5
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-19 10:07:47 -0700

    Time: 14 ms (46.49%), Space: 44.5 MB (43.54%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
+
         
         for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
+            if (Character.isLetter(c) || Character.isDigit(c)) {
                 sb.append(Character.toLowerCase(c));
             }
-            
         }
+    
+        
+        System.out.println(sb.toString());
         return sb.toString().equals(sb.reverse().toString());
+        
+        
     }
 }
