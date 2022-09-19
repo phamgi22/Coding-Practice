0daa55654af3515294d9a123550eee5b80f64ce1
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-19 02:56:30 -0700

    Time: 1 ms (99.67%), Space: 40.1 MB (97.86%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -10,10 +10,8 @@ class Solution {
             else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') stack.pop();
             else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') stack.pop();
             else return false;
-            
-            
-            
         }
+        
         
         return stack.isEmpty();
     }
