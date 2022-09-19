2063c4611c986be5a9f1e934627c798f9661c81f
Giang Pham <91588716+phamgi@users.noreply.github.com>
2022-09-19 11:46:28 -0700

    Time: 5 ms (57.53%), Space: 57.9 MB (21.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,7 +1,5 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-        
         int count = 0;
         
         for (int i = 0; i < grid.length; i++) {
@@ -16,18 +14,13 @@ class Solution {
         return count;
     }
     
-    
     public void callBFS(char[][] grid, int i, int j) {
-        if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[i].length - 1 || grid[i][j] == '0') {
-            return;
-        }
-        
+        if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[i].length - 1 || grid[i][j] == '0') return;
         
         grid[i][j] = '0';
-        
         callBFS(grid, i + 1, j);
         callBFS(grid, i - 1, j);
-        callBFS(grid, i, j + 1);
+        callBFS(grid, i , j + 1);
         callBFS(grid, i, j - 1);
     }
 }
