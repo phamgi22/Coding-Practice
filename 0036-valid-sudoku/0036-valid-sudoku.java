620d42a986fea0fda92ae5ee5bb7fe627cd0d035
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-01-25 14:58:59 -0800

    Time: 15 ms (32.29%), Space: 42.6 MB (66.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen = new HashSet<>();
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char current = board[i][j];
-                if (current != '.') {
-                    if(!seen.add(current + " found in row " + i) ||
-                    !seen.add(current + " found in column " + j) ||
-                    !seen.add(current + " found in subbox " + i/3 + "-" + j/3)) return false;
+        Set<String> unique = new HashSet<>();
+        
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                char current = board[r][c];
+                if (board[r][c] != '.') {
+                    if (!unique.add(current + " found at row " + r)) return false;
+                    if (!unique.add(current + " found at col " + c)) return false;
+                    if (!unique.add(current + " found at box " + r/(board.length/3) + c/(board.length/3))) return false;
                     
                 }
             }
         }
         
         return true;
+        
     }
 }
