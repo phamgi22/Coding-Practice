ace0385565bf0d2b6c3533a9a2561d701323c25a
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-01-26 15:10:09 -0800

    Time: 4 ms (79.08%), Space: 43.3 MB (56.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                sb.append(c);
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            char start = s.charAt(i);
+            char end = s.charAt(j);
+            if (!Character.isLetterOrDigit(start)) {
+                i++;
+                continue;
             }
+            if (!Character.isLetterOrDigit(end)) {
+                j--;
+                continue;
+            }
+            if (Character.toLowerCase(start) != Character.toLowerCase(end)) return false;
+            i++;
+            j--;
+
         }
         
-        String sbString = sb.toString().toLowerCase();
-        String sbStringReverse = sb.reverse().toString().toLowerCase();
-        
-        return sbString.equals(sbStringReverse);
+        return true;
     }
 }
