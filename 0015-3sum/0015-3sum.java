b87a056973eca9572379331a1ae598984612da32
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-01-27 17:32:20 -0800

    Time: 39 ms (54.39%), Space: 50.2 MB (42.13%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,7 +4,7 @@ class Solution {
         List<List<Integer>> list = new ArrayList<>();
         
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || i > 0 && nums[i] != nums[i-1]) {
+            if (i == 0 || i > 0 && nums[i]  != nums[i-1]) {
                 int target = 0 - nums[i];
                 int left = i + 1;
                 int right = nums.length - 1;
@@ -18,10 +18,9 @@ class Solution {
                         list.add(mini);
                         
                         while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1 ]) right--;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
                         left++;
                         right--;
- 
                     } else if (nums[left] + nums[right] > target) {
                         right--;
                     } else {
