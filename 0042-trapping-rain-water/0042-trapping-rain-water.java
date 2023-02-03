d9b2213297f058647d7630f2979d2edd3f68d7c4
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-03 01:32:48 -0800

    Time: 14 ms (11.05%), Space: 43.4 MB (37.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -5,16 +5,26 @@ class Solution {
         int max = height[0];
         int count = 0;
         
-        for (int i = 0; i < height.length; i++) {
-            left[i] = Math.max(height[i], max);
+        // max 0
+        // height 0 1 0 2 1 0 1 3 2 1 2 1
+        // lef    0
+        left[0] = 0;
+        
+        for (int i = 1; i < height.length; i++) {
+            left[i] = Math.max(height[i - 1], max);
             max = left[i];
         }
         
+        System.out.println(Arrays.toString(left));
+        
+        right[height.length - 1] = 0;
         max = height[height.length - 1];
         for (int i = height.length - 1; i >= 0; i--) {
             right[i] = Math.max(height[i], max);
             max = right[i];
         }
+        
+        System.out.println(Arrays.toString(right));
         
         for (int i = 0; i < height.length; i++) {
             if (Math.min(left[i], right[i]) - height[i] > 0) {
