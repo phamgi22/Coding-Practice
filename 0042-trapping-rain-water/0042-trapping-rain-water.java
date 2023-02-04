e4c61261fa2d114c1085ddcd2bb0dc0f9e7cd0c5
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-03 17:25:36 -0800

    Time: 1 ms (98.56%), Space: 43 MB (60.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,34 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        int left[] = new int[height.length];
-        int right[] = new int[height.length];
-        int max = height[0];
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
         int count = 0;
         
-        // max 0
-        // height 0 1 0 2 1 0 1 3 2 1 2 1
-        // lef    0
-        left[0] = 0;
-        
-        for (int i = 1; i < height.length; i++) {
-            left[i] = Math.max(height[i - 1], max);
-            max = left[i];
-        }
-        
-        
-        right[height.length - 1] = 0;
-        max = height[height.length - 1];
-        for (int i = height.length - 2; i >= 0; i--) {
-            right[i] = Math.max(height[i + 1], max);
-            max = right[i];
-        }
-        
-        
-        for (int i = 0; i < height.length; i++) {
-            if (Math.min(left[i], right[i]) - height[i] > 0) {
-             count += Math.min(left[i], right[i]) - height[i];
-            // System.out.println(count);
-
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax,height[left]);
+                count += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax,height[right]);
+                count  += rightMax - height[right];
             }
         }
         
