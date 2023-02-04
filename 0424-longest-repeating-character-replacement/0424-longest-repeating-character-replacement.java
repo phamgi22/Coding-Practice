9cd80a3ecc4dd31007136aa2f19a0840b44f4a74
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-03 19:47:47 -0800

    Time: 6 ms (80.73%), Space: 42 MB (76.95%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,22 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] arr = new int[26];
-        int ans = 0;
-        int max = 0;
-        int i = 0;
+        int[] arr = new int[26]; // store count of each 26 character
+        int longest = 0; // will return longest length of substring with only 1 repeating character
+        int max = 0; // Max count of character that appear the most so far
+        int l = 0; // left pointer
         
-        for (int j = 0; j < s.length(); j++) {
-            arr[s.charAt(j) - 'A']++;
-            max = Math.max(max, arr[s.charAt(j) - 'A']);
+        for (int r = 0; r < s.length(); r++) { // incrementing right pointer
+            arr[s.charAt(r) - 'A']++; // counting everytime each character appear
+            max = Math.max(max, arr[s.charAt(r) - 'A']); // updating the max count of character that appear the most
             
-            if (j - i + 1 - max > k) {
-                arr[s.charAt(i) - 'A']--;
-                i++;
+            if (r - l + 1 - max > k) {
+                arr[s.charAt(l) - 'A']--;
+                l++;
             }
             
-            ans = Math.max(ans, j - i + 1);
+            longest = Math.max(longest, r - l + 1);
         }
         
-        return ans;
+        return longest;
     }
 }
