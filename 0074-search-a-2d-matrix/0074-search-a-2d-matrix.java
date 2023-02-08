3554df1519eb65172c887a09925e4fa27181afff
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-07 19:34:33 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (90.47%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == target) return true;
-            }
+        if (matrix.length == 0) return false;
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int l = 0;
+        int r = row * col - 1;
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int mid_e = matrix[mid/col][mid%col];
+            if (mid_e == target) return true;
+            else if (target < mid_e) r = mid - 1;
+            else l = mid + 1;
         }
         
         return false;
