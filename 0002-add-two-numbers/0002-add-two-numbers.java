50dbc1cb36569548d850595a191270935c21cf37
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-16 22:34:39 -0800

    Time: 2 ms (94.94%), Space: 42.5 MB (43.12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,55 +1,33 @@
-/*
-    Name - Add Two Numbers
-    Link - https://leetcode.com/problems/add-two-numbers/
-    Time Complexity - O(m + n)
-    Space Complexity - o(1)
-    Note - make use of modulo (get remainder) and division (get quotient)
-*/
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
-    public ListNode addTwoNumbers(ListNode first, ListNode second) {
-        int q = 0;
-        int r = 0;
-        int sum = 0;
-        ListNode head = null;
-        ListNode temp = null;
-        while (first != null || second != null) {
-            sum =
-                q +
-                (
-                    ((first != null) ? first.val : 0) +
-                    ((second != null) ? second.val : 0)
-                );
-            r = sum % 10;
-            q = sum / 10;
-            ListNode newNode = new ListNode(r);
-            if (head == null) {
-                head = newNode;
-            } else {
-                temp = head;
-                while (temp.next != null) {
-                    temp = temp.next;
-                }
-                temp.next = newNode;
-                newNode.next = null;
-            }
-            if (first != null) {
-                first = first.next;
-            }
-            if (second != null) {
-                second = second.next;
-            }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur  =  dummy;
+        
+        int carry = 0;
+        while (l1 != null || l2  != null || carry != 0) {
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+            
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            int digit = sum % 10;
+            cur.next = new ListNode(digit);
+            
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        if (q > 0) {
-            ListNode newNode = new ListNode(q);
-            temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-            newNode.next = null;
-        }
-        return head;
+        
+        return dummy.next;
     }
 }
