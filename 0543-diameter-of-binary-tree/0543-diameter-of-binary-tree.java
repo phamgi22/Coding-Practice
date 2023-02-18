0a7c55b31993a3d467fbd0fa9d52090c08086b42
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-17 23:44:32 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (80.21%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,29 +14,18 @@
  * }
  */
 class Solution {
-    
-    int res = -1;
+    int dia = -1;
     public int diameterOfBinaryTree(TreeNode root) {
-        /*
-        Question:
-        - find the longest distance between two nodes
-        
-        Solving:
-        - brute force: for every node, traverse left and right to find the distance beween the lowest node on both side
-        - bottom up
-        */
-        
         dfs(root);
-        return res;
+        return dia;
     }
-    
     
     private int dfs(TreeNode curr) {
         if (curr == null) return -1;
         
         int left = 1 + dfs(curr.left);
         int right = 1 + dfs(curr.right);
-        res = Math.max(res, left + right);
-        return Math.max(left,right);
+        dia = Math.max(dia, left + right);
+        return Math.max(left, right);
     }
 }
