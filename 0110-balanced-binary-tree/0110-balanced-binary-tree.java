be0bd3a56dc386a2f4d87256f3557f8d18a851f6
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-18 00:06:10 -0800

    Time: 1 ms (95.13%), Space: 41.6 MB (92.55%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,23 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-  // Recursively obtain the height of a tree. An empty tree has -1 height
-  private int height(TreeNode root) {
-    // An empty tree has height -1
-    if (root == null) {
-      return -1;
+    // Recursively obtain the height of a tree. An empty tree has a height of -1
+    private int height(TreeNode root) {
+        if (root == null) return -1;
+        return 1 + Math.max(height(root.left), height(root.right));
     }
-    return 1 + Math.max(height(root.left), height(root.right));
-  }
-
-  public boolean isBalanced(TreeNode root) {
-    // An empty tree satisfies the definition of a balanced tree
-    if (root == null) {
-      return true;
+    
+    
+    public boolean isBalanced(TreeNode root) {
+        // An empty tree satisfies the definiton of a balanced tree
+        
+        if (root == null) return true;
+        return Math.abs(height(root.left) - height(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+        
     }
-
-    // Check if subtrees have height within 1. If they do, check if the
-    // subtrees are balanced
-    return Math.abs(height(root.left) - height(root.right)) < 2
-        && isBalanced(root.left)
-        && isBalanced(root.right);
-  }
-};
+}
