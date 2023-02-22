8a23f25252a42c4841e0ced8453d9153f03ec6f5
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-21 20:09:51 -0800

    Time: 0 ms (100.00%), Space: 44.2 MB (6.06%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -21,8 +21,6 @@ class Solution {
     private boolean validate(TreeNode root, Integer max, Integer min) {
         if (root == null) return true;
         else if (max != null && root.val >= max || min != null && root.val <= min) return false;
-        else {
-            return validate(root.left, root.val, min) && validate(root.right, max, root.val);
-        }
+        else return validate(root.left, root.val, min) && validate(root.right, max, root.val);
     }
 }
