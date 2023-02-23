9dc0f73e08734f43f90900b0f9996b1a84a2f3ca
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-22 19:35:01 -0800

    Time: 0 ms (100.00%), Space: 45.9 MB (5.46%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,20 +15,24 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
+        // we rememeber what element we are on, which smallest element we are
+        // we remember the result that we want to return
         int[] nums = new int[2];
         inorder(root, nums, k);
+        
         return nums[1];
     }
     
+    
     private void inorder(TreeNode root, int[] nums, int k) {
         if (root == null) return;
+        
         inorder(root.left, nums, k);
         if (++nums[0] == k) {
             nums[1] = root.val;
             return;
-        }
+        } 
         
         inorder(root.right, nums, k);
-        
-    }  
+    }
 }
