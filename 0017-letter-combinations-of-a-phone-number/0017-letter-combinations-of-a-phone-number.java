be5d127468e9613ca8d4d75e73a762569b1e98f1
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-28 11:17:02 -0800

    Time: 1 ms (84.40%), Space: 41.5 MB (38.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,41 @@
 class Solution {
     private List<String> combinations = new ArrayList<>();
-    private Map<Character, String> letters = Map.of(
-        '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
-        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+    private Map<Character, String> letters = new HashMap<>();
+    public Solution() {
+        letters.put('2', "abc");
+        letters.put('3', "def");
+        letters.put('4', "ghi");
+        letters.put('5', "jkl");
+        letters.put('6', "mno");
+        letters.put('7', "pqrs");
+        letters.put('8', "tuv");
+        letters.put('9', "wxyz");
+
+    }
+    
+
     private String phoneDigits;
     
     public List<String> letterCombinations(String digits) {
-        // If the input is empty, immediately return an empty answer array
         if (digits.length() == 0) {
             return combinations;
         }
         
-        // Initiate backtracking with an empty path and starting index of 0
         phoneDigits = digits;
         backtrack(0, new StringBuilder());
         return combinations;
     }
     
     private void backtrack(int index, StringBuilder path) {
-        // If the path is the same length as digits, we have a complete combination
         if (path.length() == phoneDigits.length()) {
             combinations.add(path.toString());
-            return; // Backtrack
+            return;
         }
         
-        // Get the letters that the current digit maps to, and loop through them
         String possibleLetters = letters.get(phoneDigits.charAt(index));
         for (char letter: possibleLetters.toCharArray()) {
-            // Add the letter to our current path
             path.append(letter);
-            // Move on to the next digit
             backtrack(index + 1, path);
-            // Backtrack by removing the letter before moving onto the next
             path.deleteCharAt(path.length() - 1);
         }
     }
