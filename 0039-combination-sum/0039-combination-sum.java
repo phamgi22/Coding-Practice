301f9dcfae4e8f870c2ef5467867c1181b4f07ae
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-28 10:33:41 -0800

    Time: 2 ms (88.91%), Space: 43 MB (29.40%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void backtrack(int remain, LinkedList<Integer> comb, int start, int[] candidates, List<List<Integer>> results) {
+        if (remain == 0) {
+            results.add(new ArrayList<>(comb)); 
+            return;
+        } else if (remain < 0) return;
+        
+        for (int i = start; i < candidates.length; i++) {
+            comb.add(candidates[i]);
+            this.backtrack(remain - candidates[i], comb, i, candidates, results);
+            comb.removeLast();
+        }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        LinkedList<Integer> comb = new LinkedList<Integer>();
+        this.backtrack(target, comb, 0, candidates, results);
+        return results;
+        
+    }
+}
