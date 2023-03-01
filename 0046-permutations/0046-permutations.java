9be956945f2a73bd3e652939f14694f5b108c33b
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-02-28 17:12:18 -0800

    Time: 1 ms (90.94%), Space: 42.9 MB (26.46%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,21 @@
 class Solution {
     public void backtrack(int n, List<Integer> nums, List<List<Integer>> output, int first) {
-        // if all integers are used up
         if (first == n) output.add(new ArrayList<Integer>(nums));
+        
         for (int i = first; i < n; i++) {
+            // place i-th integer first in the current permutation
             Collections.swap(nums, first, i);
+            // we use the next integer to complete the permutation
             backtrack(n, nums, output, first + 1);
+            // backtrack
             Collections.swap(nums, first, i);
         }
     }
-    
-    
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> output = new ArrayList<>();
-        ArrayList<Integer> nums_1st = new ArrayList<Integer>();
-        for (int num : nums) {
-            nums_1st.add(num);
-        }
-        
-        int n = nums.length;
-        backtrack(n, nums_1st, output, 0);
+        List<Integer> permutation = new ArrayList<>();
+        for (int num : nums) permutation.add(num);
+        backtrack(nums.length, permutation, output, 0);
         return output;
     }
 }
