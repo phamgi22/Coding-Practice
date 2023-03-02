0b4fed5363997dff2ea5087357f343f334b1d598
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-02 13:31:42 -0800

    Time: 1 ms (100.00%), Space: 41.7 MB (73.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,32 +1,32 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         if (grid == null || grid.length == 0) return -1;
-        for (int i = 0; i < grid.length; i++) {
+        for (int i  = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 2) rot(grid, i, j, 2);
             }
         }
         
-        int minutes = 2;
-        for (int[] row : grid) {
-            for (int cell : row) {
-                if (cell == 1) return -1;
-                minutes = Math.max(minutes, cell);
+        int minutes = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) return -1;
+                grid[i][j] -= 2;
+                minutes = Math.max(minutes, grid[i][j]);
             }
         }
         
-        return minutes - 2;
-    }
+        return minutes;
+    } 
     
     
     private void rot(int[][] grid, int i, int j, int minutes) {
         if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[i].length - 1 || 1 < grid[i][j] && grid[i][j] < minutes || grid[i][j] == 0) return;
-        else {
-            grid[i][j] = minutes;
-            rot(grid, i, j + 1, minutes + 1);
-            rot(grid, i, j - 1, minutes + 1);
-            rot(grid, i + 1, j, minutes + 1);
-            rot(grid, i - 1, j, minutes + 1);
-        }
+        
+        grid[i][j] = minutes;
+        rot(grid, i + 1, j, minutes + 1);
+        rot(grid, i - 1, j, minutes + 1);
+        rot(grid, i, j + 1, minutes + 1);
+        rot(grid, i, j - 1, minutes + 1);
     }
 }
