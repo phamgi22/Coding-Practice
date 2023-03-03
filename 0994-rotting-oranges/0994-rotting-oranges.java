0ebd52d04522d326d600faa15d24170e3766a066
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-02 17:54:31 -0800

    Time: 1 ms (100.00%), Space: 42.2 MB (32.78%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,9 +1,11 @@
 class Solution {
+    int n = 0;
     public int orangesRotting(int[][] grid) {
         if (grid == null || grid.length == 0) return -1;
         for (int i  = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 2) rot(grid, i, j, 2);
+        
             }
         }
         
@@ -21,7 +23,11 @@ class Solution {
     
     
     private void rot(int[][] grid, int i, int j, int minutes) {
+        n++;
+       
         if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[i].length - 1 || 1 < grid[i][j] && grid[i][j] < minutes || grid[i][j] == 0) return;
+        
+        
         
         grid[i][j] = minutes;
         rot(grid, i + 1, j, minutes + 1);
