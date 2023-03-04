70a3dc61d46bfaa9e70924f0eb2a5f9de3ad6478
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-03 17:04:08 -0800

    Time: 1 ms (100.00%), Space: 42.2 MB (33.20%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,33 +1,27 @@
 class Solution {
-    int n = 0;
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid.length == 0) return -1;
-        for (int i  = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j  < grid[i].length; j++) {
                 if (grid[i][j] == 2) rot(grid, i, j, 2);
-        
             }
         }
+        
         
         int minutes = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) return -1;
+                if (grid[i][j]  == 1) return -1;
                 grid[i][j] -= 2;
                 minutes = Math.max(minutes, grid[i][j]);
             }
         }
         
         return minutes;
-    } 
+    }
     
     
     private void rot(int[][] grid, int i, int j, int minutes) {
-        n++;
-       
         if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[i].length - 1 || 1 < grid[i][j] && grid[i][j] < minutes || grid[i][j] == 0) return;
-        
-        
         
         grid[i][j] = minutes;
         rot(grid, i + 1, j, minutes + 1);
