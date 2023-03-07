659a70f37b15ebb31731dc211f0bb73dad8edc6d
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-07 14:12:05 -0800

    Time: 1 ms (99.33%), Space: 44.4 MB (57.21%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,34 +1,83 @@
+// class Solution {
+//     public void solve(char[][] board) {
+//         if (board.length == 0 || board[0].length == 0) return;
+//         int row = board.length;
+//         int col = board[0].length;
+//         for (int i = 0; i < row; i++) {
+//             if (board[i][0] == '0') boundaryDFS(board, i, 0);
+//             if (board[i][col - 1] == '0') boundaryDFS(board, i, col - 1);
+//         }
+        
+//         for (int j = 0; j < col; j++) {
+//             if (board[0][j] == '0') boundaryDFS(board, 0, j);
+//             if (board[row - 1][j] == '0') boundaryDFS(board, row - 1, j);
+//         }
+        
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (board[i][j] == '0') board[i][j] = 'X';
+//                 else if (board[i][j] == '*') board[i][j] = '0';
+//             }
+//         }
+//     }
+    
+//     public void boundaryDFS(char[][] board, int i, int j) {
+//         if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length) return;
+//         if (board[i][j] == '0') board[i][j] = '*';
+//         if (i > 0 && board[i-1][j] == '0') boundaryDFS(board, i - 1, j);
+//         if (i < board.length - 1 && board[i+1][j] == '0') boundaryDFS(board, i + 1, j);
+//         if (j > 0 && board[i][j - 1] == '0') boundaryDFS(board, i, j - 1);
+//         if (j < board[0].length - 1 && board[i][j+1] == '0') boundaryDFS(board, i, j + 1);
+        
+//         return;
+//     }
+// }
+
 class Solution {
-    public void solve(char[][] board) {
-    // Check top and bottom rows
-    for (int j = 0; j < board[0].length; j++) {
-        if (board[0][j] == 'O') validate(board, 0, j);
-        if (board[board.length-1][j] == 'O') validate(board, board.length-1, j);
-    }
 
-    // Check left and right columns
-    for (int i = 0; i < board.length; i++) {
-        if (board[i][0] == 'O') validate(board, i, 0);
-        if (board[i][board[0].length-1] == 'O') validate(board, i, board[0].length-1);
-    }
-
-    // Mark remaining 'O' cells as 'X'
-    for (int i = 0; i < board.length; i++) {
-        for (int j = 0; j < board[0].length; j++) {
-            if (board[i][j] == 'O') board[i][j] = 'X';
-            if (board[i][j] == '1') board[i][j] = 'O'; // Restore marked cells
-        }
-    }
+public void solve(char[][] board) {
+	if (board.length == 0 || board[0].length == 0)
+		return;
+	if (board.length < 2 || board[0].length < 2)
+		return;
+	int m = board.length, n = board[0].length;
+	//Any 'O' connected to a boundary can't be turned to 'X', so ...
+	//Start from first and last column, turn 'O' to '*'.
+	for (int i = 0; i < m; i++) {
+		if (board[i][0] == 'O')
+			boundaryDFS(board, i, 0);
+		if (board[i][n-1] == 'O')
+			boundaryDFS(board, i, n-1);	
+	}
+	//Start from first and last row, turn '0' to '*'
+	for (int j = 0; j < n; j++) {
+		if (board[0][j] == 'O')
+			boundaryDFS(board, 0, j);
+		if (board[m-1][j] == 'O')
+			boundaryDFS(board, m-1, j);	
+	}
+	//post-prcessing, turn 'O' to 'X', '*' back to 'O', keep 'X' intact.
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] == 'O')
+				board[i][j] = 'X';
+			else if (board[i][j] == '*')
+				board[i][j] = 'O';
+		}
+	}
 }
-
-private void validate(char[][] board, int i, int j) {
-    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') return;
-
-    board[i][j] = '1'; // Mark cell as visited
-    validate(board, i+1, j); // Check down
-    validate(board, i-1, j); // Check up
-    validate(board, i, j+1); // Check right
-    validate(board, i, j-1); // Check left
-}
-
-}
+//Use DFS algo to turn internal however boundary-connected 'O' to '*';
+private void boundaryDFS(char[][] board, int i, int j) {
+	if (i < 0 || i > board.length - 1 || j <0 || j > board[0].length - 1)
+		return;
+	if (board[i][j] == 'O')
+		board[i][j] = '*';
+	if (i > 1 && board[i-1][j] == 'O')
+		boundaryDFS(board, i-1, j);
+	if (i < board.length - 2 && board[i+1][j] == 'O')
+		boundaryDFS(board, i+1, j);
+	if (j > 1 && board[i][j-1] == 'O')
+		boundaryDFS(board, i, j-1);
+	if (j < board[i].length - 2 && board[i][j+1] == 'O' )
+		boundaryDFS(board, i, j+1);
+}}
