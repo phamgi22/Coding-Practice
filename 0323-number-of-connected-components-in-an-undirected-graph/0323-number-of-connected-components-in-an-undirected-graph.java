03419a528d10bc344424ebbcce59ff91e772253a
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-09 13:45:43 -0800

    Time: 1 ms (100.00%), Space: 43.2 MB (23.39%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,39 +1,33 @@
 class Solution {
     private int[] parent;
     private int[] rank;
-    
     public int countComponents(int n, int[][] edges) {
         parent = new int[n];
         rank = new int[n];
-        
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
             rank[i] = i;
+            parent[i] = i;
         }
         
         int result = n;
         for (int i = 0; i < edges.length; i++) {
-            if (union(edges[i][0], edges[i][1]) == 1)  result--;
+            if (union(edges[i][0], edges[i][1]) == 1) result--;
         }
-        
         return result;
     }
     
-    
     private int find(int node) {
         int result = node;
-        while (parent[result] != result) {
+        while(parent[result] != result)  {
             parent[result] = parent[parent[result]];
             result = parent[result];
         }
-        
         return result;
     }
     
     private int union(int n1, int n2) {
         int p1 = find(n1);
         int p2 = find(n2);
-        
         if (p1 == p2) return 0;
         if (rank[p2] > rank[p1]) {
             parent[p1] = p2;
