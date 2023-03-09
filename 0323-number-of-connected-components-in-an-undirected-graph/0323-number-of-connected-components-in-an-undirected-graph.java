7c019a14a3e472769da651d50c56ace4ff8cb85e
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-09 13:20:28 -0800

    Time: 1 ms (100.00%), Space: 42.7 MB (47.52%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,35 +1,45 @@
-class Solution {
-    
-     private void dfs(List<Integer>[] adjList, int[] visited, int startNode) {
-        visited[startNode] = 1;
-         
-        for (int i = 0; i < adjList[startNode].size(); i++) {
-            if (visited[adjList[startNode].get(i)] == 0) {
-                dfs(adjList, visited, adjList[startNode].get(i));
-            }
+public class Solution {
+
+    private int find(int[] representative, int vertex) {
+        if (vertex == representative[vertex]) {
+            return vertex;
         }
+        
+        return representative[vertex] = find(representative, representative[vertex]);
     }
     
-    public int countComponents(int n, int[][] edges) {
-        int components = 0;
-        int[] visited = new int[n];
+    private int combine(int[] representative, int[] size, int vertex1, int vertex2) {
+        vertex1 = find(representative, vertex1);
+        vertex2 = find(representative, vertex2);
         
-        List<Integer>[] adjList = new ArrayList[n]; 
-        for (int i = 0; i < n; i++) {
-            adjList[i] = new ArrayList<Integer>();
-        }
-        
-        for (int i = 0; i < edges.length; i++) {
-            adjList[edges[i][0]].add(edges[i][1]);
-            adjList[edges[i][1]].add(edges[i][0]);
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                components++;
-                dfs(adjList, visited, i);
+        if (vertex1 == vertex2) {
+            return 0;
+        } else {
+            if (size[vertex1] > size[vertex2]) {
+                size[vertex1] += size[vertex2];
+                representative[vertex2] = vertex1;
+            } else {
+                size[vertex2] += size[vertex1];
+                representative[vertex1] = vertex2;
             }
+            return 1;
         }
+    }
+
+    public int countComponents(int n, int[][] edges) {
+        int[] representative = new int[n];
+        int[] size = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            representative[i] = i;
+            size[i] = 1;
+        }
+        
+        int components = n;
+        for (int i = 0; i < edges.length; i++) { 
+            components -= combine(representative, size, edges[i][0], edges[i][1]);
+        }
+
         return components;
     }
 }
