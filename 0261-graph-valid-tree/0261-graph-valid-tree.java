642f977a66cdb99b95fad8bf56a5960c8693e993
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-09 16:12:50 -0800

    Time: 0 ms (100.00%), Space: 43 MB (28.21%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.java
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -1,32 +1,63 @@
-class Solution {
-    public boolean validTree(int n, int[][] edges) {
-        
-    if (edges.length != n - 1) return false;
+class UnionFind {
     
-    // Make the adjacency list.
-    List<List<Integer>> adjacencyList = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-        adjacencyList.add(new ArrayList<>());
-    }
-    for (int[] edge : edges) {
-        adjacencyList.get(edge[0]).add(edge[1]);
-        adjacencyList.get(edge[1]).add(edge[0]);
-    }
+    private int[] parent;
     
-    Stack<Integer> stack = new Stack<>();
-    Set<Integer> seen = new HashSet<>();
-    stack.push(0);
-    seen.add(0);
-    
-    while (!stack.isEmpty()) {
-        int node = stack.pop();
-        for (int neighbour : adjacencyList.get(node)) {
-            if (seen.contains(neighbour)) continue;
-            seen.add(neighbour);
-            stack.push(neighbour);
+    // For efficiency, we aren't using makeset, but instead initialising
+    // all the sets at the same time in the constructor.
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int node = 0; node < n; node++) {
+            parent[node] = node;
         }
     }
     
-    return seen.size() == n;   
+    // The find method, without any optimizations. It traces up the parent
+    // links until it finds the root node for A, and returns that root.
+    public int find(int A) {
+        while (parent[A] != A) {
+            A = parent[A];
+        }
+        return A;
+    }
+
+    // The union method, without any optimizations. It returns True if a
+    // merge happened, False if otherwise.
+    public boolean union(int A, int B) {
+        // Find the roots for A and B.
+        int rootA = find(A);
+        int rootB = find(B);
+        // Check if A and B are already in the same set.
+        if (rootA == rootB) {
+            return false;
+        }
+        // Merge the sets containing A and B.
+        parent[rootA] = rootB;
+        return true;
+    } 
 }
+
+class Solution {
+    
+    public boolean validTree(int n, int[][] edges) {
+        
+        // Condition 1: The graph must contain n - 1 edges.
+        if (edges.length != n - 1) return false;
+        
+        // Condition 2: The graph must contain a single connected component.
+        // Create a new UnionFind object with n nodes. 
+        UnionFind unionFind = new UnionFind(n);
+        // Add each edge. Check if a merge happened, because if it 
+        // didn't, there must be a cycle.
+        for (int[] edge : edges) {
+            int A = edge[0];
+            int B = edge[1];
+            if (!unionFind.union(A, B)) {
+                return false;
+            }
+        }
+        
+        // If we got this far, there's no cycles!
+        return true;
+    }
+    
 }
