77726ed1091fd544b5e941cbd92e6439c36e0078
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-13 21:48:59 -0700

    Time: 2 ms (97.02%), Space: 59.2 MB (85.47%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int position = 0;
+        int sum = 0;
+        int total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                total += sum;
+                sum = 0;
+                
+                position = i + 1;
+            }
+        }
+        
+        total += sum;
+        return total >= 0 ? position : -1;
+    }
+}
