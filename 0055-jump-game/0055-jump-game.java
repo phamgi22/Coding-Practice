a331a95dcfdd1b53d3d765100dca98ec36b199bd
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-13 21:30:07 -0700

    Time: 1 ms (99.99%), Space: 43 MB (38.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int lastGoodIndexPosition = nums.length - 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i + nums[i] >= lastGoodIndexPosition) {
-                lastGoodIndexPosition = i;
-            }
+        int goodIndex = nums.length - 1;
+        for (int i = nums.length - 1; i>= 0; i--) {
+            if (i + nums[i] >= goodIndex) goodIndex = i;
         }
         
-        return lastGoodIndexPosition == 0;
+        return goodIndex == 0;
     }
 }
