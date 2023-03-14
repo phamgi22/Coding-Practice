7750e9d032293125994a74ec3af7befa7bdd3801
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-14 16:46:32 -0700

    Time: 56 ms (45.80%), Space: 44.2 MB (22.84%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -6,16 +6,16 @@ class Solution {
         
         for (int card : hand) {
             if (!card_counts.containsKey(card)) card_counts.put(card, 1);
-            else card_counts.put(card, card_counts.get(card) + 1);
+            else card_counts.replace(card, card_counts.get(card) + 1);
         }
         
         while (card_counts.size() > 0) {
             int min = card_counts.firstKey();
-            for (int card = min; card <= min + groupSize - 1; card++) {
+            for (int card = min; card < min + groupSize; card++) {
                 if (!card_counts.containsKey(card)) return false;
                 int count = card_counts.get(card);
                 if (count == 1) card_counts.remove(card);
-                else card_counts.put(card, count - 1);
+                else card_counts.replace(card, count - 1);
             }
         }
         
