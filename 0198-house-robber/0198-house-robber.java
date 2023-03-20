24963f8f11d0f22451b419cffd8a949ad3731171
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-20 13:52:29 -0700

    Time: 0 ms (100.00%), Space: 40.3 MB (20.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,34 +1,15 @@
 class Solution {
-    
-    private int[] memo;
-    
     public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
         
-        this.memo = new int[100];
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
         
-        // Fill with sentinel value representing not-calculated recursions.
-        Arrays.fill(this.memo, -1);
-        
-        return this.robFrom(0, nums);
-    }
-    
-    private int robFrom(int i, int[] nums) {
-        
-        // No more houses left to examine.
-        if (i >= nums.length) {
-            return 0;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i + 1] = Math.max(dp[i], dp[i-1] + nums[i]);
         }
         
-        // Return cached value.
-        if (this.memo[i] > -1) {
-            return this.memo[i];
-        }
-        
-        // Recursive relation evaluation to get the optimal answer.
-        int ans = Math.max(this.robFrom(i + 1, nums), this.robFrom(i + 2, nums) + nums[i]);
-        
-        // Cache for future use.
-        this.memo[i] = ans;
-        return ans;
+        return dp[nums.length];
     }
 }
