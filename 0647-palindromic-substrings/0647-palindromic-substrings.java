f98801a53a8ba95b61368f85eb8a3dfb6dc4a822
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-20 21:21:41 -0700

    Time: 3 ms (96.72%), Space: 40.2 MB (89.52%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,32 +1,23 @@
 class Solution {
     public int countSubstrings(String s) {
         int ans = 0;
-
-        for (int i = 0; i < s.length(); ++i) {
-            // odd-length palindromes, single character center
-            ans += countPalindromesAroundCenter(s, i, i);
-
-            // even-length palindromes, consecutive characters center
-            ans += countPalindromesAroundCenter(s, i, i + 1);
+        for (int i = 0; i < s.length(); i++) {
+            ans += count(s, i, i);
+            ans += count(s, i, i + 1);
         }
-
         return ans;
     }
-
-    private int countPalindromesAroundCenter(String ss, int lo, int hi) {
+    
+    private int count(String s, int left, int right) {
         int ans = 0;
-
-        while (lo >= 0 && hi < ss.length()) {
-            if (ss.charAt(lo) != ss.charAt(hi))
-                break;      // the first and last characters don't match!
-
-            // expand around the center
-            lo--;
-            hi++;
-
+        while (left >= 0
+              && right <= s.length() - 1
+              && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
             ans++;
         }
-
+        
         return ans;
     }
 }
