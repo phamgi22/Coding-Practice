1994e6207e6ddab619695c740e2341aaaaecda27
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-20 21:17:50 -0700

    Time: 11 ms (44.51%), Space: 42.8 MB (24.29%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,22 +1,28 @@
 class Solution {
-    private boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start) != s.charAt(end)) 
-                return false;
+    public int countSubstrings(String s) {
+        int n = s.length(), ans = 0;
 
-            ++start;
-            --end;
+        if (n <= 0) 
+            return 0;
+
+        boolean[][] dp = new boolean[n][n];
+
+        // Base case: single letter substrings
+        for (int i = 0; i < n; ++i, ++ans) 
+            dp[i][i] = true;
+
+        // Base case: double letter substrings
+        for (int i = 0; i < n - 1; ++i) {
+            dp[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
+            ans += (dp[i][i + 1] ? 1 : 0);
         }
 
-        return true;
-    }
-
-    public int countSubstrings(String s) {
-        int ans = 0;
-
-        for (int start = 0; start < s.length(); ++start)
-            for (int end = start; end < s.length(); ++end) 
-                ans += isPalindrome(s, start, end) ? 1 : 0;
+        // All other cases: substrings of length 3 to n
+        for (int len = 3; len <= n; ++len)
+            for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
+                dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+                ans += (dp[i][j] ? 1 : 0);
+            }
 
         return ans;
     }
