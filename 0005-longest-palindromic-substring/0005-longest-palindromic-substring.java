dff23aa2bc9c60f0eb5067a59873c00ce950fe1c
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-20 20:55:35 -0700

    Time: 24 ms (62.33%), Space: 42.7 MB (48.81%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -7,31 +7,25 @@ class Solution {
             // racecar case
             int len1 = expand(s, i, i);
             // baab case
-            int len2 = expand(s, i, i + 1);
-            
+            int len2 = expand(s, i, i + 1);   
             int len = Math.max(len1, len2);
-            
             if (len > end - start) {
                 start = i - ((len - 1) / 2);
                 end = i + (len / 2);
             }
         }
-        
-        
         return s.substring(start, end + 1);
         
     }
     
     private int expand(String s, int left, int right) {
         if (s == null || left > right) return 0;
-        
         while (left >= 0 
                && right <= s.length() - 1 
                && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        
         return right - left - 1;
     }
 }
