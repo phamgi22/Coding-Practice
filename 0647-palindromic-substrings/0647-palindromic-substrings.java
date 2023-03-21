d182fe78e2f6effedf209ad9ab048393ba322376
Giang Pham <91588716+phamgi@users.noreply.github.com>
2023-03-20 21:24:35 -0700

    Time: 3 ms (96.72%), Space: 40.9 MB (52.24%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,6 +1,7 @@
 class Solution {
     public int countSubstrings(String s) {
         int ans = 0;
+        if (s.length() == 0 || s == null) return ans;
         for (int i = 0; i < s.length(); i++) {
             ans += count(s, i, i);
             ans += count(s, i, i + 1);
@@ -10,14 +11,11 @@ class Solution {
     
     private int count(String s, int left, int right) {
         int ans = 0;
-        while (left >= 0
-              && right <= s.length() - 1
-              && s.charAt(left) == s.charAt(right)) {
+        while (left >= 0 && right <= s.length() - 1 && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
             ans++;
         }
-        
         return ans;
     }
 }
