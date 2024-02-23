092b1595d73abc42b41f44f4bd4817b886794248
Giang Pham <91588716+phamgi22@users.noreply.github.com>
2024-02-23 01:21:15 -0800

    Time: 47 ms (71.37%), Space: 16.9 MB (81.09%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        smap = {}
+        tmap = {}
+        
+        for i in range(len(s)):
+            char = s[i]
+            smap[char] = smap.get(char, 0) + 1
+        
+        for i in range(len(t)):
+            char = t[i]
+            tmap[char] = tmap.get(char, 0) + 1
+        
+        for key in smap:
+            if key not in tmap or tmap[key] != smap[key]:
+                return False
+    
+        print(smap)
+        print(tmap)
+        
+        return True
+        
