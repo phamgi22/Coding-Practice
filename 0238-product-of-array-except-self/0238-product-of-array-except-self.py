72bc6fdf65a2431da3bd3afe1dbd035ab324ead7
Giang Pham <91588716+phamgi22@users.noreply.github.com>
2024-02-23 18:24:46 -0800

    Time: 168 ms (56.04%), Space: 23.8 MB (85.78%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,17 +1,35 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left = [1] * len(nums)
-        right = [1] * len(nums)
+        res = [1] * len(nums)
+        left = 1
         
-        for i in range(1, len(nums)):
-            left[i] = left[i - 1] * nums[i - 1]
+        for i in range(0, len(nums)):
+            res[i] = left
+            left *= nums[i]
+            
+
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+            
+        
+        return res
         
         
-        for i in range(len(nums) - 2, -1, -1):
-            right[i] = right[i + 1] * nums[i + 1]
+        
+#         left = [1] * len(nums)
+#         right = [1] * len(nums)
+        
+#         for i in range(1, len(nums)):
+#             left[i] = left[i - 1] * nums[i - 1]
         
         
-        for i in range(len(left)):
-            right[i] *= left[i]
+#         for i in range(len(nums) - 2, -1, -1):
+#             right[i] = right[i + 1] * nums[i + 1]
         
-        return right
+        
+#         for i in range(len(left)):
+#             right[i] *= left[i]
+        
+#         return right
