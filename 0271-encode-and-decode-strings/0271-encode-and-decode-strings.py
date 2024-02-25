28e392affabd5a1185f7f00c8736ccebb8e0caaf
Giang Pham <91588716+phamgi22@users.noreply.github.com>
2024-02-25 15:49:49 -0800

    Time: 59 ms (92.25%), Space: 17.1 MB (52.83%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -3,11 +3,12 @@ class Codec:
         """Encodes a list of strings to a single string.
         """
         res = ""
+        
         for string in strs:
             res += str(len(string)) + "#" + string
         
         return res
-        
+
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
@@ -20,9 +21,9 @@ class Codec:
                 j += 1
             
             length = int(s[i:j])
-            res .append(s[j + 1 : j + 1 + length])
+            res.append(s[j + 1 : j + 1 + length])
             i = j + 1 + length
-
+        
         return res
         
 
