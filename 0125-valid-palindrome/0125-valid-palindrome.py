07d0d3c316b2d0ce9ac8103d2a95f1e13dcf89c9
Giang Pham <91588716+phamgi22@users.noreply.github.com>
2024-02-26 22:49:58 -0800

    Time: 54 ms (22.86%), Space: 17 MB (94.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = ""
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
+        l = 0
+        r = len(s) - 1
         
+        while l < r:
+            while l < r and not self.isCharOrDig(s[l]):
+                l += 1
+            while r > l and not self.isCharOrDig(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l = l + 1
+            r = r - 1
         
-        return newStr == newStr[::-1]
+        return True
+    
+    
+    def isCharOrDig(self, c: chr) -> bool:
+        return (ord('A') <= ord(c) <= ord('Z') or
+        ord('a') <= ord(c) <= ord('z') or
+        ord('0') <= ord(c) <= ord('9'))
