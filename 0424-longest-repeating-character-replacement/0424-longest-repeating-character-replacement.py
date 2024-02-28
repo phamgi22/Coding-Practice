26d12eafa4c4148dec4b104df89e98d6eb39ace0
Giang Pham <91588716+phamgi22@users.noreply.github.com>
2024-02-27 20:59:36 -0800

    Time: 127 ms (26.16%), Space: 16.7 MB (45.49%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,17 +1,28 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
-        l = 0
+        '''
+        initialize hashmap
+        l and r pointer 
         
-        maxf = 0
+        calculate the number of char need replace x
+        if x <= k: we update the length of our max winleng, update the fre of the character at r pointer, and then update r pointer
+        if x > k: shrink the length of the the windown length by shifting l pointer to the right
+        
+        return
+        '''
+        
+        count = {}
+        l = 0
+        res = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
-            while (r - l + 1) - maxf > k:
+            while (r - l + 1) - max(count.values()) > k:
+                
                 count[s[l]] -= 1
                 l += 1
-                
+            
+            
             res = max(res, r - l + 1)
+            
         
         return res
