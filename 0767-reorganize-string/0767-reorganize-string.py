01d2458804486ebf666582741b7d254169509816
Giang Pham <91588716+phamgi22@users.noreply.github.com>
2024-02-28 01:11:09 -0800

    Time: 35 ms (76.74%), Space: 16.6 MB (86.29%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -5,33 +5,32 @@ class Solution:
         for char in s:
             hashmap[char] = 1 + hashmap.get(char, 0)
             
-        maxHeap = [[-count, char] for char, count in hashmap.items() ] 
+        maxHeap = [[-count, char] for char, count in hashmap.items()]
         
         heapq.heapify(maxHeap)
-#         print(maxHeap)
         
-        prev = None
-    
         res = ""
+        pre = None
         
-        while maxHeap or prev:
-            
-            # a -> 1
-            if prev and not maxHeap:
+        while maxHeap or pre:
+            if pre and not maxHeap:
                 return ""
             
-            # most freq except prev
+            
+            # get most frequence
             count, char = heapq.heappop(maxHeap)
-            res += char
             count += 1
+            res += char
             
-            if prev:
-                heapq.heappush(maxHeap, prev)
-                prev = None
+            # if pre is not null, mean it's storing the most fre char
+            # return it back to the heap and empty pre
+            if pre:
+                heapq.heappush(maxHeap, pre)
+                pre = None
             
-            if count < 0:
-                prev = [count, char]
             
-            
-        
+            # hide the most frequent char inside pre
+            if count != 0:
+                pre = [count, char]
+                
         return res
