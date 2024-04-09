eacd5ebf1b758b1d2e6b5e136769dce1206e5b14
Giang Pham <91588716+phamgi22@users.noreply.github.com>
2024-04-08 23:33:51 -0700

    Time: 587 ms (23.28%), Space: 0B (100.00%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+
+SELECT
+    id, movie, description, rating
+FROM
+    Cinema
+WHERE 
+    id % 2 <> 0
+    AND description <> "boring"
+ORDER BY
+    rating DESC
